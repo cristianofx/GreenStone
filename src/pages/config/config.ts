ae0b13c7a63e557cf4c1aff1bcbd7e229b4aec3f
Cristiano Matzum Guilloux da Silva <cristianofx@gmail.com>
2017-09-26 14:46:00 -0300

    Changes to config datepicker and lunch time added

--- src/pages/config/config.ts
+++ src/pages/config/config.ts
@@ -4,6 +4,9 @@ import { AlertController } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 import { HomePage } from "../home/home";
 
+import { DatePicker } from '@ionic-native/date-picker';
+import * as moment from 'moment';
+
 
 @Component({
   selector: 'config-list',
@@ -16,12 +19,17 @@ export class ConfigPage {
   timeToWork: string;
   tolerance: string;
   standardInHour: string;
+  lunchTime: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, 
-              public storage: Storage) {
+              public storage: Storage, private datePicker: DatePicker) {
 
     storage.get('timeToWork').then((val) => {
       this.timeToWork = val || '';
+    });
+
+    storage.get('lunchTime').then((val) => {
+      this.lunchTime = val || '';
     });
 
     storage.get('tolerance').then((val) => {
@@ -35,6 +43,7 @@ export class ConfigPage {
 
   showAlert() {
 
+    this.storage.set('lunchTime', this.lunchTime);
     this.storage.set('timeToWork', this.timeToWork);
     this.storage.set('tolerance', this.tolerance);
     this.storage.set('standardInHour', this.standardInHour);
@@ -47,10 +56,22 @@ export class ConfigPage {
     alert.present();
 
     this.navCtrl.setRoot(HomePage);
+  }
 
-    this.storage.get('timeToWork').then((val) => {
-      console.log('The time is', val);
-    });
-    
+  nativePicker(field: string){
+    let parent = this;
+    this.datePicker.show({
+      date: new Date(),
+      mode: 'time',
+      is24Hour: true,
+      androidTheme: this.datePicker.ANDROID_THEMES.THEME_DEVICE_DEFAULT_LIGHT
+    }).then(
+      date => {
+        let hourInPicker = moment(date);
+        let hour = hourInPicker.format('HH:mm');
+        parent[field] = hour;
+      },
+      err => console.log('Error occurred while getting date: ', err)
+    );
   }
 }
